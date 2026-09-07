486471c8b661b0093d5eb96bab12e9e6206dcc8f
ryuto <ryuto.kiwako@gmail.com>
2026-09-07 21:19:04 +0900

    Add Google Search Console verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="-6F3LTgXvahm20I78efx05okehy3xS2EXjMuyjmrWRY" />
 
     <title>@yield('title', 'イベント運営アプリ')</title>
 
